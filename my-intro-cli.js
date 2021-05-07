8ec6244b2413627c3b5baad237d544baff0c2fea
skullKim <space1878@naver.com>
2021-05-07 22:15:32 +0900

    done

---
+++ my-intro-cli.js
@@ -0,0 +1,7 @@
+var container = document.getElementById('map');
+var options = {
+    center: new kakao.maps.LatLng(37.56600625596156, 126.95135801148976),
+    level: 3
+};
+
+var map = new kakao.maps.Map(container, options);
